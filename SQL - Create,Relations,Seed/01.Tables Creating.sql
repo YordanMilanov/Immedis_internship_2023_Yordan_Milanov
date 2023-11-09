cbff6bd176c0e989e9af80a15f02a04444f7d1b3
YordanMilanov <dancho.galinov@gmail.com>
2023-11-09 16:36:26 +0200

    final update

---
+++ SQL - Create,Relations,Seed/01.Tables Creating.sql
@@ -0,0 +1,115 @@
+ --Create Database HCMSImmedis;
+ --use HCMSImmedis;
+
+--TABLES
+
+--Authentication
+CREATE TABLE "Users" (
+    "Id" UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+	"Username" VARCHAR(50) NOT NULL UNIQUE,
+	"Password" VARCHAR(1000) NOT NULL,
+	"Email" VARCHAR(100) NOT NULL UNIQUE,
+	"RegisterDate" DATETIME NOT NULL
+);
+
+--Roles
+CREATE TABLE "Roles" (
+    "Id" INT PRIMARY KEY,
+    "Name" VARCHAR(50) NOT NULL UNIQUE,
+	"Description" varchar(max) NULL,
+);
+
+--Info about Workers / Employees
+CREATE TABLE "Employees"(
+	"Id" UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+	"FirstName" VARCHAR(50) NOT NULL,
+    "LastName" VARCHAR(50) NOT NULL,
+	"Email" VARCHAR(100) NOT NULL UNIQUE,
+	"PhoneNumber" VARCHAR(50) NOT NULL UNIQUE,
+	"PhotoURL" varchar(max) NULL,
+    "DateOfBirth" DATETIME NOT NULL,
+	"AddDate" DATETIME NOT NULL,
+	"CompanyId" UNIQUEIDENTIFIER NULL,
+	"UserId" UNIQUEIDENTIFIER NULL,
+	"LocationId" UNIQUEIDENTIFIER NULL,
+);
+
+-- Info about employers / companies
+CREATE TABLE "Companies" (
+	"Id" UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+    "Name" VARCHAR(50) NOT NULL UNIQUE,
+	"Description" varchar(max) NULL,
+	"IndustryField" VARCHAR(50) NULL,
+	"LocationId" UNIQUEIDENTIFIER NULL,
+);
+
+-- Info about experience of particular employee
+CREATE TABLE "WorkRecords" (
+	"Id" UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+	"Position" VARCHAR(50) NOT NULL,
+	"Department" VARCHAR(50) NULL,
+    "Salary" DECIMAL(19, 2) NOT NULL,
+	"StartDate" DATETIME NOT NULL,
+	"EndDate" DATETIME NULL, --If Null - currently at that company
+	"AddDate" DATETIME NOT NULL,
+	"CompanyId" UNIQUEIDENTIFIER NOT NULL,
+	"EmployeeId" UNIQUEIDENTIFIER NOT NULL,
+);
+
+CREATE TABLE "Applications" (
+	"Id" UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+	"Position" VARCHAR(50) NOT NULL,
+	"Department" VARCHAR(50) NOT NULL,
+    "CoverLetter" varchar(max) NULL,
+	"AddDate" DATETIME NOT NULL,
+	"ToCompanyId" UNIQUEIDENTIFIER NOT NULL,
+	"FromEmployeeId" UNIQUEIDENTIFIER NOT NULL,
+);
+
+-- Info about particular education of an employee 
+CREATE TABLE "Educations" (
+	"Id" UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+	"StartDate" DATETIME NOT NULL,
+	"EndDate" DATETIME NULL,
+	"Degree" VARCHAR(50) NOT NULL,
+	"FieldOfEducation" VARCHAR(50) NOT NULL,
+	"University" VARCHAR(50) NOT NULL,
+	"Grade" DECIMAL(19,2) NOT NULL,
+	"EmployeeId" UNIQUEIDENTIFIER NOT NULL,
+	"LocationId" UNIQUEIDENTIFIER NULL,
+);
+
+-- Recommendation about employee directed to specific company
+CREATE TABLE "Recommendations" (
+	"Id" UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+	"Description" varchar(max) NOT NULL,
+	"RecommendDate" DATETIME NOT NULL,
+	"ForEmployeeId" UNIQUEIDENTIFIER NOT NULL,
+	"ToCompanyId" UNIQUEIDENTIFIER NOT NULL,
+);
+
+--Location(Address) of specific entity
+CREATE TABLE "Locations" (
+	"Id"UNIQUEIDENTIFIER PRIMARY KEY, --GUID
+	"Address" VARCHAR(50) NULL,
+	"State" VARCHAR(50) NOT NULL,
+	"Country" VARCHAR(50) NOT NULL,
+	OwnerId UNIQUEIDENTIFIER NOT NULL,
+    OwnerType VARCHAR(255) NOT NULL
+);
+
+--Mapping table (User - Roles)
+CREATE TABLE "UsersRoles" (
+	"UserId" UNIQUEIDENTIFIER, --GUID
+	"RoleId" INT,
+	PRIMARY KEY (UserId, RoleId)
+);
+
+--User Claims 
+CREATE TABLE "UsersClaims" (
+	"Id" UNIQUEIDENTIFIER, --GUID
+	"UserId" UNIQUEIDENTIFIER,
+	"ClaimType" VARCHAR(255) NOT NULL,
+	"ClaimValue" varchar(max)
+);
+
---
+++ SQL - Create,Relations,Seed/02.Table Relations.sql
@@ -0,0 +1,74 @@
+--use Immedis;
+--use HCMSImmedis;
+
+--FKs
+
+--One to One Employees-Users
+ALTER TABLE "Employees"
+ADD CONSTRAINT "FK_Employees_Users"
+FOREIGN KEY (UserId) REFERENCES Users(Id),
+UNIQUE (UserId);
+
+--Many to One Employees-Locations
+ALTER TABLE "Employees"
+ADD CONSTRAINT "FK_Employees_Locations"
+FOREIGN KEY (LocationId) REFERENCES Locations(Id);
+
+--Many to One Companies-Locations
+ALTER TABLE "Companies"
+ADD CONSTRAINT "FK_Companies_Locations"
+FOREIGN KEY (LocationId) REFERENCES Locations(Id);
+
+--Many to One Records-Locations
+ALTER TABLE "WorkRecords"
+ADD CONSTRAINT "FK_WorkRecords_Companies"
+FOREIGN KEY (CompanyId) REFERENCES Companies(Id);
+
+--Many to One Records-Employees
+ALTER TABLE "WorkRecords"
+ADD CONSTRAINT "FK_WorkRecords_Employees"
+FOREIGN KEY (EmployeeId) REFERENCES Employees(Id);
+
+--Many to One Applications-Companies
+ALTER TABLE "Applications"
+ADD CONSTRAINT "FK_Applications_Companies"
+FOREIGN KEY (ToCompanyId) REFERENCES Companies(Id);
+
+--Many to One Applications-Employees
+ALTER TABLE "Applications"
+ADD CONSTRAINT "FK_Applications_Employees"
+FOREIGN KEY (FromEmployeeId) REFERENCES Employees(Id);
+
+--Many to One Educations-Locations
+ALTER TABLE "Educations"
+ADD CONSTRAINT "FK_Educations_Locations"
+FOREIGN KEY (LocationId) REFERENCES Locations(Id);
+
+--Many to One Educations-Employees
+ALTER TABLE "Educations"
+ADD CONSTRAINT "FK_Educations_Employees"
+FOREIGN KEY (EmployeeId) REFERENCES Employees(Id);
+
+--Many to One Recommendations-Employees
+ALTER TABLE "Recommendations"
+ADD CONSTRAINT "FK_Recommendations_Employees"
+FOREIGN KEY (ForEmployeeId) REFERENCES Employees(Id);
+
+--Many to One Recommendations-Employees
+ALTER TABLE "Recommendations"
+ADD CONSTRAINT "FK_Recommendations_Companies"
+FOREIGN KEY (ToCompanyId) REFERENCES Companies(Id);
+
+--Many to Many User - UserRoles - Roles
+ALTER TABLE "UsersRoles"
+ADD CONSTRAINT "FK_Users_Roles"
+FOREIGN KEY (UserId) REFERENCES Users(Id);
+
+ALTER TABLE "UsersRoles"
+ADD CONSTRAINT "FK_Roles_Users"
+FOREIGN KEY (RoleId) REFERENCES Roles(Id);
+
+--Many to One Users-UsersClaims
+ALTER TABLE "UsersClaims"
+ADD CONSTRAINT "FK_Users_Claims"
+FOREIGN KEY (UserId) REFERENCES Users(Id);
