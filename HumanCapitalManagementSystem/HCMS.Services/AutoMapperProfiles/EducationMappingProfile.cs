6e249901870bf12ba25fd798920851cdb075993d
YordanMilanov <dancho.galinov@gmail.com>
2023-11-09 16:35:30 +0200

    last update

---
+++ HumanCapitalManagementSystem/HCMS.Services/AutoMapperProfiles/EducationMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HCMS.Common.Structures;
+using HCMS.Data.Models;
+using HCMS.Services.ServiceModels.Education;
+
+namespace HCMS.Web.Api.AutoMapperProfiles
+{
+    public class EducationMappingProfile : Profile
+    {
+        public EducationMappingProfile()
+        {
+            CreateMap<Education, EducationDto>()
+                .ForMember(dest => dest.Location, opt => opt.MapFrom(src => new LocationStruct(src.Location!.Address, src.Location.State, src.Location.Country)));
+
+            CreateMap<EducationDto, Education>()
+                .ForMember(dest => dest.Location, opt => opt.MapFrom(src => new Location { Address = src.Location.GetAddress(), State = src.Location.GetState(), Country = src.Location.GetCountry() }));
+        }
+    }
+}
---
+++ HumanCapitalManagementSystem/HCMS.Services/AutoMapperProfiles/WorkRecordMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HCMS.Data.Models;
+using HCMS.Data.Models.QueryPageGenerics;
+using HCMS.Services.ServiceModels.BaseClasses;
+using HCMS.Services.ServiceModels.WorkRecord;
+
+namespace HCMS.Web.Api.AutoMapperProfiles;
+public class WorkRecordMappingProfile : Profile
+{
+    public WorkRecordMappingProfile()
+    {
+        CreateMap<WorkRecord, WorkRecordDto>()
+            .ForMember(dest => dest.CompanyName,opt => opt.MapFrom(src => src.Company.Name));
+
+
+        CreateMap<WorkRecordDto, WorkRecord>()
+            .ForMember(dest => dest.CompanyId, opt => opt.Ignore())
+            .ForMember(dest => dest.Company, opt => opt.Ignore())
+            .ForMember(dest => dest.Employee, opt => opt.Ignore());
+
+        CreateMap<QueryPageWrapClass<WorkRecord>, QueryDtoResult<WorkRecordDto>>();
+    }
+}
